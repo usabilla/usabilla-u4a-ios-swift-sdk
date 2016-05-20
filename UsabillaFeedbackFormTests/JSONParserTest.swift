f444706e034eee587fa835cf79b63633bf1d3971
Giacomo Pinato <Jack@Giacomos-MacBook-Pro.local>
2016-05-20 14:45:17 +0200

    I got testing down

--- UsabillaFeedbackFormTests/JSONParserTest.swift
+++ UsabillaFeedbackFormTests/JSONParserTest.swift
@@ -6,48 +6,74 @@
 //  Copyright © 2016 Usabilla. All rights reserved.
 //
 
-import XCTest
+
+import Quick
+import Nimble
+import SwiftyJSON
+
+
+@testable import UsabillaFeedbackForm
 
 class JSONParserTest: QuickSpec {
-
+    
     override func spec() {
         
-        let stringToChunk = "abcdefghilmnopqrst"
+        var formModel: FormModel!
         
         beforeSuite {
+            let path = NSBundle(forClass: JSONParserTest.self).pathForResource("test", ofType: "json")!
+            do {
+                let data = try NSData(contentsOfURL: NSURL(fileURLWithPath: path), options: NSDataReadingOptions.DataReadingMappedIfSafe)
+                let jsonObj: JSON = JSON(data:data)
+                formModel = JSONFormParser.parseFormJson(jsonObj, appId: "a", screenshot: nil)
+                
+            } catch let error as NSError {
+                Swift.debugPrint(error.localizedDescription)
+            }
             
         }
         
-        describe("chunking a string") {
-            beforeEach {
-                //Only for this describe
+        describe("the JSON parser") {
+            context("with a valid form"){
+                beforeEach {
+                    //Only for this describe
+                }
+                
+                it("should correctly extract the form settings"){
+                    expect(formModel.appTitle).to(equal("FeedbackTest"))
+                    expect(formModel.appSubmitButton).to(equal("TestSubmit"))
+                    expect(formModel.hasScreenshot).to(equal(true))
+                    expect(formModel.version).to(equal(0))
+                    expect(formModel.appId).to(equal("a"))
+                    expect(formModel.isDefault).to(equal(false))
+                    expect(formModel.errorMessage).to(equal("Error"))
+                }
+                
+                describe("the pages array"){
+                    
+                    it("should have been correctly parsed"){
+                        let pages = formModel.pages
+                        expect(pages.count).to(equal(4))
+                    }
+                    
+                    it("should contain a valid second page"){
+                        let page = formModel.pages[1]
+                        expect(page.pageName).to(equal("middle"))
+                        expect(page.defaultJumpTo).to(equal("end"))
+                        expect(page.jumpRuleList?.count).to(equal(1))
+
+                    }
+                    
+                    
+                }
+                
             }
             
-            it("should correctly divide by 3"){
-                let stringChunks = stringToChunk.divideInChunksOfSize(3)
-                expect(stringChunks.count).to(equal(6))
-                expect(stringChunks[5].characters.count).to(equal(3))
-                expect(stringChunks[0].characters.count).to(equal(3))
-                expect(stringChunks[5]).to(equal("rst"))
-                expect(stringChunks[0]).to(equal("abc"))
-            }
-            
-            it("should correctly divide by 4"){
-                let stringChunks = stringToChunk.divideInChunksOfSize(4)
-                expect(stringChunks.count).to(equal(5))
-                expect(stringChunks[4].characters.count).to(equal(2))
-                expect(stringChunks[0].characters.count).to(equal(4))
-                expect(stringChunks[4]).to(equal("st"))
-                expect(stringChunks[0]).to(equal("abcd"))
-            }
-            
-            it("should correctly divide by 5"){
-                let stringChunks = stringToChunk.divideInChunksOfSize(5)
-                expect(stringChunks.count).to(equal(4))
-                expect(stringChunks[3].characters.count).to(equal(3))
-                expect(stringChunks[0].characters.count).to(equal(5))
-                expect(stringChunks[3]).to(equal("rst"))
-                expect(stringChunks[0]).to(equal("abcde"))
+            func testPerformanceExample() {
+                // This is an example of a performance test case.
+                self.measureBlock {
+                    //JSONFormParser.parseFormJson(jsonObj, appId: "a", screenshot: nil)
+                }
             }
         }
     }
