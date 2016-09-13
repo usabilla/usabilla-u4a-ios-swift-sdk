f061ad3f046689c4b92a2cf0ac1787ea6bc2742e
Giacomo Pinato <Jack@Giacomos-MacBook-Pro.local>
2016-09-13 21:51:47 +0200

    it compiles

--- UsabillaFeedbackForm/Utils/StarRating/StarRatingView.swift
+++ UsabillaFeedbackForm/Utils/StarRating/StarRatingView.swift
@@ -27,63 +27,6 @@ class StarRatingiView: UIControl {
         
     }
     var continuous: Bool
-    var opaque: Bool
-    
-    override init(frame: CGRect) {
-        customInit()
-        super.init(frame: frame)
-        
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        customInit()
-        super.init(coder: aDecoder)
-    }
-    
-    func customInit() {
-        self.exclusiveTouch = true
-        minimumValue = 0
-        maximumValue = 5
-        spacing = 5
-        continuous = true
-        updateAppearanceForState(enabled)
-    }
-    
-    override func setNeedsLayout() {
-        super.setNeedsLayout()
-        setNeedsDisplay()
-    }
-    
-    
-    override var backgroundColor :UIColor? {
-        get {
-            if (super.backgroundColor != nil) {
-                return super.backgroundColor
-            } else {
-                return self.opaque ? UIColor.whiteColor() : UIColor.clearColor()
-            }
-        }
-        
-        set {
-            self.backgroundColor = newValue
-        }
-    }
-    
-    
-    
-    func setValue(newValue: Int, sendValueChangedAction: Bool) {
-        //[self willChangeValueForKey:NSStringFromSelector(@selector(value))];
-        willChangeValueForKey("currentValue")
-        if (newValue >= minimumValue && newValue <= maximumValue) {
-            currentValue = newValue
-            if sendAction {
-                sendActionsForControlEvents(UIControlEvents.ValueChanged)
-            }
-            setNeedsDisplay()
-        }
-        //[self didChangeValueForKey:NSStringFromSelector(@selector(value))];
-        didChangeValueForKey("currentValue")
-    }
     
     var allowHalfStars: Bool {
         didSet{
@@ -129,8 +72,95 @@ class StarRatingiView: UIControl {
         }
     }
     
+    override init(frame: CGRect) {
+
+        minimumValue = 0
+        maximumValue = 5
+        currentValue = 0
+        spacing = 5
+        continuous = true
+        shouldBecomeFirstResponder = false
+        allowHalfStars = false
+        accurateHalfStars = true
+        super.init(frame: frame)
+        exclusiveTouch = true
+        updateAppearanceForState(enabled)
+
+
+
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        minimumValue = 0
+        maximumValue = 5
+        currentValue = 0
+        spacing = 5
+        continuous = true
+        shouldBecomeFirstResponder = false
+        allowHalfStars = false
+        accurateHalfStars = true
+        super.init(coder: aDecoder)
+        exclusiveTouch = true
+        updateAppearanceForState(enabled)
+
+
+
+    }
+    
+//    init() {
+//        exclusiveTouch = true
+//        minimumValue = 0
+//        maximumValue = 5
+//        currentValue = 0
+//        spacing = 5
+//        continuous = true
+//        updateAppearanceForState(enabled)
+//        shouldBecomeFirstResponder = false
+//        allowHalfStars = false
+//        accurateHalfStars = true
+//
+//    }
+    
+    override func setNeedsLayout() {
+        super.setNeedsLayout()
+        setNeedsDisplay()
+    }
+    
+    
+    override var backgroundColor :UIColor? {
+        get {
+            if (super.backgroundColor != nil) {
+                return super.backgroundColor
+            } else {
+                return self.opaque ? UIColor.whiteColor() : UIColor.clearColor()
+            }
+        }
+        
+        set {
+            self.backgroundColor = newValue
+        }
+    }
+    
+    
+    
+//    func setValue(newValue: Int, sendValueChangedAction: Bool) {
+//        //[self willChangeValueForKey:NSStringFromSelector(@selector(value))];
+//        willChangeValueForKey("currentValue")
+//        if (newValue >= minimumValue && newValue <= maximumValue) {
+//            currentValue = newValue
+//            if sendAction {
+//                sendActionsForControlEvents(UIControlEvents.ValueChanged)
+//            }
+//            setNeedsDisplay()
+//        }
+//        //[self didChangeValueForKey:NSStringFromSelector(@selector(value))];
+//        didChangeValueForKey("currentValue")
+//    }
+    
+    
+    
     func updateAppearanceForState(enabled: Bool){
-        self.alpha = enabled ? 1.0 : 0.5
+        alpha = enabled ? 1.0 : 0.5
     }
     
     
@@ -170,11 +200,11 @@ class StarRatingiView: UIControl {
     }
     
     func drawStarShapeWithFrame(frame: CGRect,tintColor:UIColor, highlighted:Bool) {
-        [self _drawAccurateHalfStarShapeWithFrame:frame tintColor:tintColor progress:highlighted ? 1.f : 0.f];
+        drawAccurateHalfStarShapeWithFrame(frame, tintColor: tintColor, progress: highlighted ? 1: 0)
     }
     
     func drawHalfStarShapeWithFrame(frame: CGRect, tintColor:UIColor) {
-        [self _drawAccurateHalfStarShapeWithFrame:frame tintColor:tintColor progress:.5f];
+        drawAccurateHalfStarShapeWithFrame(frame, tintColor: tintColor, progress: 0.5)
     }
     
     func drawAccurateHalfStarShapeWithFrame(frame: CGRect, tintColor:UIColor, progress:CGFloat) {
@@ -218,107 +248,105 @@ class StarRatingiView: UIControl {
         CGContextSetFillColorWithColor(context!, self.backgroundColor!.CGColor)
         CGContextFillRect(context!, rect)
         
-        let availableWidth = rect.size.width - (spacing * Float(maximumValue - 1)) - 2
-        let cellWidth = availableWidth / maximumValue
+        let availableWidth = rect.size.width - (spacing * CGFloat(maximumValue - 1)) - 2
+        let cellWidth = availableWidth / CGFloat(maximumValue)
         let starSide = (cellWidth <= rect.size.height) ? cellWidth : rect.size.height;
         
         for idx in 0...Int(maximumValue) {
             
-            let center = CGPointMake(cellWidth * idx + cellWidth/2 + spacing * idx + 1, rect.size.height/2)
+            let center = CGPointMake(cellWidth * CGFloat(idx) + cellWidth/2 + spacing * CGFloat(idx) + 1, rect.size.height/2)
             let frame = CGRectMake(center.x - starSide/2, center.y - starSide/2, starSide, starSide);
-            let highlighted = (idx + 1 <= ceilf(currentValue))
+            let highlighted = (idx + 1 <= currentValue)
             
-            [self _drawStarWithFrame:frame tintColor:self.tintColor highlighted:highlighted];
-            
+            drawStarImageWithFrame(frame, tintColor: tintColor, highlighted: highlighted)
         }
     }
     
-    func drawStarWithFrame(Cframe: GRect, tintColor:UIColor, highlighted:Bool) {
+    func drawStarWithFrame(Cframe: CGRect, tintColor:UIColor, highlighted:Bool) {
         if self.shouldUseImages {
             drawStarImageWithFrame(frame, tintColor: tintColor, highlighted: highlighted)
-            
         } else {
-            drawstarsh
-            [self _drawStarShapeWithFrame:frame tintColor:tintColor highlighted:highlighted];
-        }
-    }
-    
-    - (void)_drawHalfStarWithFrame:(CGRect)frame tintColor:(UIColor *)tintColor {
-    if (self.shouldUseImages) {
-    [self _drawHalfStarImageWithFrame:frame tintColor:tintColor];
-    } else {
-    [self _drawHalfStarShapeWithFrame:frame tintColor:tintColor];
-    }
-    }
-    - (void)_drawAccurateStarWithFrame:(CGRect)frame tintColor:(UIColor *)tintColor progress:(CGFloat)progress {
-    if (self.shouldUseImages) {
-    [self _drawAccurateHalfStarImageWithFrame:frame tintColor:tintColor progress:progress];
-    } else {
-    [self _drawAccurateHalfStarShapeWithFrame:frame tintColor:tintColor progress:progress];
-    }
-    }
-    
-    override func beginTrackingWithTouch(touch: UITouch, withEvent:UIEvent ) -> Bool{
-        if enabled {
-            super.beginTrackingWithTouch(touch, withEvent: withEvent)
-            if shouldBecomeFirstResponder && !isFirstResponder {
-                becomeFirstResponder()
-            }
-            handleTouch(touch)
+            drawStarWithFrame(frame, tintColor: tintColor, highlighted: highlighted)
             
-            return true
-        } else {
-            return false
         }
     }
     
-    func continueTrackingWithTouch(touch :UITouch, withEvent:UIEvent) ->Bool {
-        if enabled{
-            super.continueTrackingWithTouch(touch, withEvent: withEvent)
-            handleTouch(touch)
-            return true
-        } else {
-            return false
-        }
-    }
+    //    - (void)_drawHalfStarWithFrame:(CGRect)frame tintColor:(UIColor *)tintColor {
+    //    if (self.shouldUseImages) {
+    //    [self _drawHalfStarImageWithFrame:frame tintColor:tintColor];
+    //    } else {
+    //    [self _drawHalfStarShapeWithFrame:frame tintColor:tintColor];
+    //    }
+    //    }
+    //    - (void)_drawAccurateStarWithFrame:(CGRect)frame tintColor:(UIColor *)tintColor progress:(CGFloat)progress {
+    //    if (self.shouldUseImages) {
+    //    [self _drawAccurateHalfStarImageWithFrame:frame tintColor:tintColor progress:progress];
+    //    } else {
+    //    [self _drawAccurateHalfStarShapeWithFrame:frame tintColor:tintColor progress:progress];
+    //    }
+    //    }
+    
+//    func beginTrackingWithTouch(touch: UITouch, withEvent:UIEvent ) -> Bool{
+//        if enabled {
+//            super.beginTrackingWithTouch(touch, withEvent: withEvent)
+//            if shouldBecomeFirstResponder && !isFirstResponder() {
+//                becomeFirstResponder()
+//            }
+//            handleTouch(touch)
+//            
+//            return true
+//        } else {
+//            return false
+//        }
+//    }
+//    
+//    
+//    func continueTrackingWithTouch(touch :UITouch, withEvent:UIEvent) ->Bool {
+//        if enabled{
+//            super.continueTrackingWithTouch(touch, withEvent: withEvent)
+//            handleTouch(touch)
+//            return true
+//        } else {
+//            return false
+//        }
+//    }
     
     
-    func endTrackingWithTouch(touch :UITouch, withEvent :UIEvent ) {
-        super.endTrackingWithTouch(touch, withEvent: withEvent)
-        
-        if (shouldBecomeFirstResponder && self.isFirstResponder()) {
-            resignFirstResponder()
-        }
-        handleTouch(touch)
-        if !continuous {
-            
-            [self sendActionsForControlEvents:UIControlEventValueChanged];
-        }
-    }
-    
-    func cancelTrackingWithEvent(event :UIEvent ) {
-        [super cancelTrackingWithEvent:event];
-        
-        if (shouldBecomeFirstResponder && isFirstResponder()) {
-            resignFirstResponder()
-        }
-    }
-    
-    - (BOOL)gestureRecognizerShouldBegin:(UIGestureRecognizer *)gestureRecognizer {
-    if ([gestureRecognizer.view isEqual:self]) {
-    return !self.isUserInteractionEnabled;
-    }
-    return self.shouldBeginGestureRecognizerBlock ? self.shouldBeginGestureRecognizerBlock(gestureRecognizer) : NO;
-    }
+//    func endTrackingWithTouch(touch :UITouch, withEvent :UIEvent ) {
+//        super.endTrackingWithTouch(touch, withEvent: withEvent)
+//        
+//        if (shouldBecomeFirstResponder && self.isFirstResponder()) {
+//            resignFirstResponder()
+//        }
+//        handleTouch(touch)
+//        if !continuous {
+//            sendActionsForControlEvents(.ValueChanged)
+//        }
+//    }
+//    
+//    func cancelTrackingWithEvent(event :UIEvent ) {
+//        super.cancelTrackingWithEvent(event)
+//        
+//        if (shouldBecomeFirstResponder && isFirstResponder()) {
+//            resignFirstResponder()
+//        }
+//    }
+//    
+//    override func gestureRecognizerShouldBegin(gestureRecognizer :UIGestureRecognizer)  -> Bool{
+//        if gestureRecognizer.view?.isEqual(self) {
+//            return !self.userInteractionEnabled
+//        }
+//        return self.shouldBeginGestureRecognizerBlock ? self.shouldBeginGestureRecognizerBlock(gestureRecognizer) : false
+//    }
     
     func handleTouch( touch:UITouch ) {
-        let cellWidth = self.bounds.size.width / maximumValue;
+        let cellWidth = self.bounds.size.width / CGFloat(maximumValue)
         let location = touch.locationInView(self)
         let value = location.x / cellWidth
         
-        currentValue = ceilf(currentValue);
+        currentValue = Int(value)
         
-        setValue(currentValue, sendValueChangedAction: continuous)
+//        setValue(currentValue, sendValueChangedAction: continuous)
         
     }
     
@@ -329,7 +357,7 @@ class StarRatingiView: UIControl {
     
     override func intrinsicContentSize() -> CGSize {
         let height = 44.0
-        return CGSizeMake(maximumValue * height + (maximumValue - 1) * spacing, height)
+        return CGSizeMake(CGFloat(maximumValue) * CGFloat(height) + CGFloat(maximumValue - 1) * spacing, CGFloat(height))
     }
     
 }
