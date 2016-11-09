f1d71d0a7b80d40a4be7f58f7c024e518f40f05b
Giacomo Pinato <Jack@Giacomos-MacBook-Pro.local>
2016-11-09 11:26:43 +0100

    new network manager

--- UsabillaFeedbackForm/Utils/NetworkManager.swift
+++ UsabillaFeedbackForm/Utils/NetworkManager.swift
@@ -12,175 +12,250 @@ import Alamofire
 
 
 class NetworkManager {
-
-
-    static let bundle = NSBundle(forClass: NetworkManager.self)
+    
+    
+    
+    static let bundle =  Bundle(for: NetworkManager.self)
     static let api_url = bundle.infoDictionary!["USABILLA_API_URL"] as! String
     static let submit_url = bundle.infoDictionary!["USABILLA_SUBMIT_ENDPOINT"] as! String
-
+    
+    
+    
+    /// Gets the json of the corrisponding form ID from the Usabilla API
+    ///
+    /// - Parameter formID: ID of the form
+    /// - Returns: Promise containing the JSON description of the form, error if form not found
     class func getFormWithFormID(formID: String) -> Promise<JSON> {
-
+        
         let headers: [String: String]? = [
-            "app-version": NSBundle.mainBundle().infoDictionary!["CFBundleVersion"] as! String,
-            "app-name": NSBundle.mainBundle().infoDictionary![kCFBundleNameKey as String] as! String,
-            "sdk-version": NSBundle(identifier: "com.usabilla.UsabillaFeedbackForm")!.objectForInfoDictionaryKey("CFBundleShortVersionString") as! String,
+            "app-version": Bundle.main.infoDictionary!["CFBundleVersion"] as! String,
+            "app-name": Bundle.main.infoDictionary![kCFBundleNameKey as String] as! String,
+            "sdk-version": Bundle(identifier: "com.usabilla.UsabillaFeedbackForm")!.object(forInfoDictionaryKey: "CFBundleShortVersionString") as! String,
             "os": "iOS"
         ]
-
-
+        
+        
         let request_url = String(format: "https://%@/live/mobile/app/forms/%@", arguments: [api_url, formID])
         return Promise { fulfill, reject in
-
-            Alamofire.request(.GET, request_url, parameters: nil, encoding: .URL, headers: headers)
-                .response { (request, response, json, error) in
-                    if let response = response {
-                        let statusCode = response.statusCode
-                        if error != nil {
-                            return reject(error!)
-                        }
-
-                        if statusCode < 200 || statusCode > 299 {
-                            return reject(NSError(domain: "Invalid FormID", code: statusCode, userInfo: [:]))
-                        }
-
-                        let json = JSON(data: json!)
-                        fulfill(json)
-                    } else {
+            
+            Alamofire.request(request_url, headers: headers)
+                .responseJSON { response in
+                    debugPrint(response)
+                    switch response.result {
+                    case .success:
+                        fulfill(response.result.value as! JSON)
+                    case .failure:
                         return reject(NSError(domain: "Invalid Request", code: 0, userInfo: [:]))
                     }
             }
-
+            
+            
+            
+            //                .response { (request, response, json, error) in
+            //                    if let response = response {
+            //                        let statusCode = response.statusCode
+            //                        if error != nil {
+            //                            return reject(error!)
+            //                        }
+            //
+            //                        if statusCode < 200 || statusCode > 299 {
+            //                            return reject(NSError(domain: "Invalid FormID", code: statusCode, userInfo: [:]))
+            //                        }
+            //
+            //                        let json = JSON(data: json!)
+            //                        fulfill(json)
+            //                    } else {
+            //                        return reject(NSError(domain: "Invalid Request", code: 0, userInfo: [:]))
+            //                    }
+            //            }
+            
         }
-
+        
     }
-
-    class func submitFormToUsabilla (payload: [String:AnyObject], screenshot: String?) {
-
-        submitFeedbackSmallData(payload).then { (response: Response<AnyObject, NSError>?) -> () in
-
-            if let response = response {
-                switch response.result {
-                case .Success(let data):
-                    let json = JSON(data)
-                    let id = json["id"].stringValue
-                    let signature = json["sig"].stringValue
-                    if let screenshot = screenshot {
-                        submitFeedbackScreenshot(id, signature: signature, screenshot: screenshot)
-                    }
-                    break
-                default :
-                    print("Request failed with error: \(response.response?.statusCode)")
-                    break
+    
+    
+    
+    /// Submits the form results to usabilla
+    ///
+    /// - Parameters:
+    ///   - payload: Payload containing the user data
+    ///   - screenshot: self explanatory screenshot
+    class func submitFormToUsabilla (payload: [String:Any], screenshot: String?) {
+        
+        
+        submitFeedbackSmallData(payload: payload).then { (response: DataResponse<Any>) -> () in
+            
+            switch response.result {
+            case .success(let data):
+                let json = JSON(data)
+                let id = json["id"].stringValue
+                let signature = json["sig"].stringValue
+                if let screenshot = screenshot {
+                    submitFeedbackScreenshot(id: id, signature: signature, screenshot: screenshot)
                 }
+                break
+            default :
+                print("Request failed with error: \(response.response?.statusCode)")
+                break
             }
-
+            }.catch { error in
+                print(error)
         }
-
-
+        //            if let response = response {
+        //                switch response.result {
+        //                case .Success(let data):
+        //                    let json = JSON(data)
+        //                    let id = json["id"].stringValue
+        //                    let signature = json["sig"].stringValue
+        //                    if let screenshot = screenshot {
+        //                        submitFeedbackScreenshot(id, signature: signature, screenshot: screenshot)
+        //                    }
+        //                    break
+        //                default :
+        //                    print("Request failed with error: \(response.response?.statusCode)")
+        //                    break
+        //                }
+        //            }
+        
     }
-
-
+    
+    
+    
+    /// Submits the screenshot as a separate request
+    ///
+    /// - Parameters:
+    ///   - id: ID of the data that the screenshot must be added to. (Widget server data)
+    ///   - signature: Same as ID
+    ///   - screenshot: base64 representation of the screenshot
     class func submitFeedbackScreenshot(id: String, signature: String, screenshot: String) {
         let chuckSize = 31250
         var promiseArray: [Promise<Bool>] = []
         let stringChunks = screenshot.divideInChunksOfSize(chuckSize)
-
-        for (index, chunk) in stringChunks.enumerate() {
-            promiseArray.append(createPromise(id, signature: signature, v: index + 1, screenshot: chunk))
+        
+        for (index, chunk) in stringChunks.enumerated() {
+            promiseArray.append(createPromise(id: id, signature: signature, v: index + 1, screenshot: chunk))
         }
-
-        when(promiseArray).then { _ -> () in
-            closeTheDeal(id, signature: signature, v: stringChunks.count + 1)
+        
+        when(resolved: promiseArray).then { _ in
+            closeTheDeal(id: id, signature: signature, v: stringChunks.count + 1)
+            }.catch { error in
+                print(error)
         }
-
+        
     }
-
+    
     class func closeTheDeal(id: String, signature: String, v: Int) -> Promise<Bool> {
-        var contentDictionary: [String: AnyObject] = [:]
+        var contentDictionary: [String: Any] = [:]
         contentDictionary["media"] = ["screenshot" : ""]
-
-        var payload: [String: AnyObject] = [:]
-
+        
+        var payload: [String: Any] = [:]
+        
         payload["id"] = id
         payload["sig"] = signature
         payload["type"] = "app_feedback"
         payload["subtype"] = "media.screenshot"
-        payload["v"] = NSNumber(int: Int32(v))
+        payload["v"] = NSNumber(value: v)
         payload["done"] = true
         payload["data"] = contentDictionary
-
+        
         return Promise { fulfill, reject in
-            Alamofire.request(.POST, submit_url, parameters: payload, encoding: .JSON)
-                .response {request, response, data, error in
-                    if let response = response {
-                        let statusCode = response.statusCode
-                        if error != nil {
-                            return reject(error!)
-                        }
-
-                        if statusCode < 200 || statusCode > 299 {
-                            return reject(NSError(domain: "Invalid FormID", code: statusCode, userInfo: [:]))
-                        }
+            
+            Alamofire.request(submit_url, method: .post, parameters: payload, encoding: JSONEncoding.default, headers: nil)
+                .responseJSON {response in
+                    switch response.result {
+                    case .success:
                         fulfill(true)
-                    } else {
+                    case .failure:
                         return reject(NSError(domain: "Invalid response", code: 0, userInfo: [:]))
                     }
+                    
+                    //                    if let response = response {
+                    //                        let statusCode = response.statusCode
+                    //                        if error != nil {
+                    //                            return reject(error!)
+                    //                        }
+                    //
+                    //                        if statusCode < 200 || statusCode > 299 {
+                    //                            return reject(NSError(domain: "Invalid FormID", code: statusCode, userInfo: [:]))
+                    //                        }
+                    //                        fulfill(true)
+                    //                    } else {
+                    //                        return reject(NSError(domain: "Invalid response", code: 0, userInfo: [:]))
+                    //                    }
             }
         }
-
-
+        
+        
     }
-
+    
     class func createPromise(id: String, signature: String, v: Int, screenshot: String) -> Promise<Bool> {
-
-        var contentDictionary: [String: AnyObject] = [:]
+        
+        var contentDictionary: [String: Any] = [:]
         contentDictionary["media"] = ["screenshot" : screenshot]
-        var payload: [String: AnyObject] = [:]
-
+        var payload: [String: Any] = [:]
+        
         payload["id"] = id
         payload["sig"] = signature
         payload["type"] = "app_feedback"
         payload["subtype"] = "media.screenshot"
-        payload["v"] = NSNumber(int: Int32(v))
+        payload["v"] = NSNumber(value: Int32(v))
         payload["done"] = false
         payload["data"] = contentDictionary
-
+        
+        
+        
         return Promise { fulfill, reject in
-            Alamofire.request(.POST, submit_url, parameters: payload, encoding: .JSON)
-                .response {request, response, data, error in
-                    if let response = response {
-                        let statusCode = response.statusCode
-                        if error != nil {
-                            return reject(error!)
-                        }
-
-                        if statusCode < 200 || statusCode > 299 {
-                            return reject(NSError(domain: "Invalid FormID", code: statusCode, userInfo: [:]))
-                        }
-
+            
+            Alamofire.request(submit_url, method: HTTPMethod.post, parameters: payload, encoding:  JSONEncoding.default, headers: nil)
+                .responseJSON { response in
+                    debugPrint(response)
+                    switch response.result {
+                    case .success(_):
                         fulfill(true)
-                    } else {
+                        
+                    case .failure(_):
                         return reject(NSError(domain: "Invalid FormID", code: 0, userInfo: [:]))
                     }
             }
+            
+            
+            //                .response {request, response, data, error in
+            //                    if let response = response {
+            //                        let statusCode = response.statusCode
+            //                        if error != nil {
+            //                            return reject(error!)
+            //                        }
+            //
+            //                        if statusCode < 200 || statusCode > 299 {
+            //                            return reject(NSError(domain: "Invalid FormID", code: statusCode, userInfo: [:]))
+            //                        }
+            //
+            //                        fulfill(true)
+            //                    } else {
+            //                        return reject(NSError(domain: "Invalid FormID", code: 0, userInfo: [:]))
+            //                    }
+            //            }
         }
-
+        
     }
-
-
-    class func submitFeedbackSmallData(payload: [String:AnyObject])  -> Promise<Response<AnyObject, NSError>?> {
+    
+    
+    /// Submits form data without screenshot (Only text)
+    ///
+    /// - Parameter payload: data to submit
+    /// - Returns: Promise containig the responde data from the widget server
+    class func submitFeedbackSmallData(payload: [String:Any])  -> Promise<DataResponse<Any>> {
         return Promise { fulfill, reject in
-            Alamofire.request(.POST, submit_url, parameters: payload, encoding: .JSON)
+            
+            Alamofire.request(submit_url, method: .post, parameters: payload, encoding: JSONEncoding.default, headers: nil)
                 .responseJSON { response in
                     switch response.result {
-                    case .Success:
+                    case .success:
                         fulfill(response)
-                        print("Validation Successful")
-                    case .Failure(let error):
-                        print(error)
+                    case .failure:
                         return reject(NSError(domain: "Invalid FormID", code: 0, userInfo: [:]))
                     }
-
+                    
             }
         }
     }
