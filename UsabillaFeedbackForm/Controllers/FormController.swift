407a02ddd2b44ee09af9ff02ad3565a9cc3cffe6
Giacomo Pinato <Jack@Giacomos-MacBook-Pro.local>
2016-11-09 13:55:05 +0100

    removed useless stuff

--- UsabillaFeedbackForm/Controllers/FormController.swift
+++ UsabillaFeedbackForm/Controllers/FormController.swift
@@ -7,31 +7,6 @@
 //
 
 import UIKit
-// FIXME: comparison operators with optionals were removed from the Swift Standard Libary.
-// Consider refactoring the code to use the non-optional operators.
-fileprivate func < <T : Comparable>(lhs: T?, rhs: T?) -> Bool {
-  switch (lhs, rhs) {
-  case let (l?, r?):
-    return l < r
-  case (nil, _?):
-    return true
-  default:
-    return false
-  }
-}
-
-// FIXME: comparison operators with optionals were removed from the Swift Standard Libary.
-// Consider refactoring the code to use the non-optional operators.
-fileprivate func > <T : Comparable>(lhs: T?, rhs: T?) -> Bool {
-  switch (lhs, rhs) {
-  case let (l?, r?):
-    return l > r
-  default:
-    return rhs < lhs
-  }
-}
-
-
 
 class FormViewController: UIViewController {
 
@@ -172,7 +147,7 @@ class FormViewController: UIViewController {
 
         transition(from: pageController, to: thankYouController, duration: 0.5, options: .transitionCrossDissolve, animations: nil, completion: nil)
 
-        thankYouController.setUpController(moodValue?.fieldValue > 3, thankTitle: headerFieldValue, thankMessage: thanksFieldValue)
+        thankYouController.setUpController((moodValue?.fieldValue)! > 3, thankTitle: headerFieldValue, thankMessage: thanksFieldValue)
 
     }
 
