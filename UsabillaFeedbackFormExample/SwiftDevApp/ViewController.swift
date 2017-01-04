059c5c733972484bb2cdfddb7ffda473c68e7b0c
Benjamin Grima <benjamin@usabilla.com>
2017-01-04 13:28:35 +0100

    Remove warnings

--- UsabillaFeedbackFormExample/SwiftDevApp/ViewController.swift
+++ UsabillaFeedbackFormExample/SwiftDevApp/ViewController.swift
@@ -10,12 +10,12 @@ import UIKit
 import UsabillaFeedbackForm
 
 class ViewController: UIViewController, UsabillaFeedbackFormDelegate {
-    
-    
-    let themeConfig =  UsabillaThemeConfigurator()
+
+
+    let themeConfig = UsabillaThemeConfigurator()
     var customVariables: [String: Any] = [:]
     var configurator: UsabillaThemeConfigurator!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -23,57 +23,53 @@ class ViewController: UIViewController, UsabillaFeedbackFormDelegate {
         UsabillaFeedbackForm.showCancelButton = true
         UsabillaFeedbackForm.appStoreId = "asdasd"
         //UsabillaFeedbackForm.localizedStringFile = "asd"
-        
+
     }
-    
-    
+
+
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        
+
         customVariables["customerId"] = "As123Bc"
         customVariables["happynessRating"] = 100
-        
+
         let off = UIImage(named: "catoff")!
         let on = UIImage(named: "caton")!
-        
+
         themeConfig.fullStar = UIImage(named: "caton")
         themeConfig.emptyStar = UIImage(named: "catoff")
-        themeConfig.enabledEmoticons = [on,on,on,on,on]
-        themeConfig.disabledEmoticons = [off,off,off,off,off]
+        themeConfig.enabledEmoticons = [on, on, on, on, on]
+        themeConfig.disabledEmoticons = [off, off, off, off, off]
         //themeConfig.customFont = UIFont(name: "batang", size: 12)
-        
-        
+
+
         UsabillaFeedbackForm.hideGiveMoreFeedback = false
-        
-        configurator =  UsabillaThemeConfigurator()
+
+        configurator = UsabillaThemeConfigurator()
         configurator.titleColor = UIColor.orange
         configurator.textColor = UIColor.black
         configurator.accentColor = UIColor.orange
         configurator.textOnAccentColor = UIColor.blue
         configurator.errorColor = UIColor.red
         configurator.statusBarColor = UIStatusBarStyle.lightContent
-        
-        
+
+
     }
-    
+
     @IBAction func otherShow(_ sender: AnyObject) {
         UsabillaFeedbackForm.loadFeedbackForm("583c0d8ea935028022c145f4", screenshot: UsabillaFeedbackForm.takeScreenshot(self.view), customVariables: customVariables)
     }
-    
+
     @IBAction func show(_ sender: Any) {
         UsabillaFeedbackForm.loadFeedbackForm("57ea3cacaaba75e6addebf7b", screenshot: nil, customVariables: customVariables, themeConfig: configurator)
     }
-    
-    
+
+
     func formFailedLoading(_ backupForm: UINavigationController) {
         present(backupForm, animated: true, completion: nil)
     }
-    
+
     func formLoadedCorrectly(_ form: UINavigationController, active: Bool) {
         present(form, animated: true, completion: nil)
     }
-
-    
 }
-
-
