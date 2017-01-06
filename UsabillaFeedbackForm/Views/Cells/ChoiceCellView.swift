13089a2ecb9aad629078a3526133f9a9e75e0d71
Benjamin Grima <benjamin@usabilla.com>
2017-01-06 13:40:45 +0100

    Implement new design for choice picker

--- UsabillaFeedbackForm/Views/Cells/ChoiceCellView.swift
+++ UsabillaFeedbackForm/Views/Cells/ChoiceCellView.swift
@@ -9,72 +9,130 @@
 import Foundation
 import UIKit
 
-class ChoiceCellView: RootCellView, UIPickerViewDelegate, UIPickerViewDataSource {
-
-    var choiceModel: ChoiceFieldModel!
-    let picker: UIPickerView
+class ChoiceCellView: RootCellView {
+    
+    var choiceModel: ChoiceFieldModel?
+    let picker = UIPickerView()
+    let pickerButton = UIButton()
+    let topBorder = UIView()
+    let bottomBorder = UIView()
+    let pickerHeightConstraint: NSLayoutConstraint
 
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
-        picker = UIPickerView()
+
+        pickerHeightConstraint = picker.heightAnchor.constraint(equalToConstant: 0)
+        pickerHeightConstraint.isActive = true
+        
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        rootCellContainerView.addSubview(topBorder)
+        rootCellContainerView.addSubview(pickerButton)
+        rootCellContainerView.addSubview(bottomBorder)
+        rootCellContainerView.addSubview(picker)
+        
+        //top border
+        topBorder.translatesAutoresizingMaskIntoConstraints = false
+        topBorder.topAnchor.constraint(equalTo: rootCellContainerView.topAnchor).isActive = true
+        topBorder.trailingAnchor.constraint(equalTo: rootCellContainerView.trailingAnchor).isActive = true
+        topBorder.leadingAnchor.constraint(equalTo: rootCellContainerView.leadingAnchor).isActive = true
+        topBorder.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        
+        // button
+        pickerButton.translatesAutoresizingMaskIntoConstraints = false
+        pickerButton.topAnchor.constraint(equalTo: topBorder.bottomAnchor).isActive = true
+        pickerButton.trailingAnchor.constraint(equalTo: rootCellContainerView.trailingAnchor).isActive = true
+        pickerButton.leadingAnchor.constraint(equalTo: rootCellContainerView.leadingAnchor).isActive = true
+        
+        pickerButton.addTarget(self, action: #selector(ChoiceCellView.pickerButtonClicked), for: .touchUpInside)
+        
+        // bottom border
+        bottomBorder.translatesAutoresizingMaskIntoConstraints = false
+        bottomBorder.topAnchor.constraint(equalTo: pickerButton.bottomAnchor).isActive = true
+        bottomBorder.trailingAnchor.constraint(equalTo: rootCellContainerView.trailingAnchor).isActive = true
+        bottomBorder.leadingAnchor.constraint(equalTo: rootCellContainerView.leadingAnchor).isActive = true
+        bottomBorder.heightAnchor.constraint(equalToConstant: 1).isActive = true
+
+        
+        // picker
         picker.dataSource = self
         picker.delegate = self
-        self.contentView.addSubview(picker)
         picker.showsSelectionIndicator = true
         picker.translatesAutoresizingMaskIntoConstraints = false
-
-        addConstraintToFillContainerView(view: picker)
-
+        
+        picker.topAnchor.constraint(equalTo: bottomBorder.bottomAnchor).isActive = true
+        picker.bottomAnchor.constraint(equalTo: rootCellContainerView.bottomAnchor).isActive = true
+        picker.trailingAnchor.constraint(equalTo: rootCellContainerView.trailingAnchor).isActive = true
+        picker.leadingAnchor.constraint(equalTo: rootCellContainerView.leadingAnchor).isActive = true
+        
+        
     }
-
+    
     override func applyCustomisations() {
         super.applyCustomisations()
-        picker.backgroundColor = choiceModel.themeConfig.backgroundColor
-        picker.tintColor = choiceModel.themeConfig.textColor
-    }
+        guard let theme = choiceModel?.themeConfig else {
+            return
+        }
+        pickerButton.setTitleColor(theme.textColor, for: .normal)
+        pickerButton.setTitle(choiceModel?.options.first?.title, for: .normal)
+        pickerButton.titleLabel?.font =  theme.font.withSize(theme.titleFontSize)
 
+        picker.backgroundColor = theme.backgroundColor
+        picker.tintColor = theme.textColor
+        bottomBorder.backgroundColor = theme.hintColor
+        topBorder.backgroundColor = theme.hintColor
+
+        
+    }
+    
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
-
+    
+    func pickerButtonClicked(){
+        UIView.animate(withDuration: 0.33) {
+            self.pickerHeightConstraint.isActive = !self.pickerHeightConstraint.isActive
+            SwiftEventBus.postToMainThread("updateMySize")
+        }
+    }
+    
     override func setFeedbackItem(_ item: FieldModelProtocol) {
         super.setFeedbackItem(item)
-
-        choiceModel = item as! ChoiceFieldModel
-        if choiceModel.fieldValue.isEmpty {
-            choiceModel.fieldValue = [choiceModel.options[0].value]
+        
+        choiceModel = item as? ChoiceFieldModel
+        guard let model = choiceModel else {
+            return
+        }
+        if model.fieldValue.isEmpty {
+            model.fieldValue = [model.options[0].value]
         } else {
-            for (index, option) in choiceModel.options.enumerated() {
-                if option.value == choiceModel.fieldValue[0] {
+            for (index, option) in model.options.enumerated() {
+                if option.value == model.fieldValue[0] {
                     picker.selectRow(index, inComponent: 0, animated: false)
                 }
             }
         }
-
-        if choiceModel.options.count < 5 {
+        
+        if model.options.count < 5 {
             let a = NSLayoutConstraint(item: picker, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 1, constant: 100)
-                a.priority = 750
-                a.isActive = true
+            a.priority = 750
+            a.isActive = true
         }
         
         picker.selectRow(0, inComponent: 0, animated: true)
     }
+    
+}
 
+extension ChoiceCellView: UIPickerViewDataSource {
+    
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
-
-
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return choiceModel.options.count
-    }
-
-//    func pickerView(_ pickerView: UIPickerView, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString? {
-//        let asd = NSAttributedString(string: choiceModel.options[row].title, attributes: [NSForegroundColorAttributeName: choiceModel.themeConfig.textColor, NSFontAttributeName: themeConfig.font.withSize(30)])
-//        return asd
-//    }
     
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return choiceModel?.options.count ?? 0
+    }
     
     func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
         
@@ -87,21 +145,19 @@ class ChoiceCellView: RootCellView, UIPickerViewDelegate, UIPickerViewDataSource
             pickerLabel?.textColor = themeConfig.textColor
         }
         
-        pickerLabel?.text = choiceModel.options[row].title
-        //pickerLabel?.sizeToFit()
-        
+        pickerLabel?.text = choiceModel?.options[row].title
         return pickerLabel!
     }
+    
+}
 
-
-
+extension ChoiceCellView: UIPickerViewDelegate {
+    
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        choiceModel.fieldValue = [choiceModel.options[row].value]
-        Swift.debugPrint("picked \(choiceModel.fieldValue)")
-
+        if let item = choiceModel?.options[row] {
+            choiceModel?.fieldValue = [item.value]
+            pickerButton.setTitle(item.title, for: .normal)
+            Swift.debugPrint("picked \(item.value)")
+        }
     }
-
-//    deinit {
-//        print("choice cell deinit")
-//    }
 }
