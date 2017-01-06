0c274910823094b3782aa31758d3a73aa3fad935
Giacomo Pinato <Jack@Giacomos-MacBook-Pro.local>
2017-01-06 16:23:49 +0100

    fixes wrong color

--- UsabillaFeedbackForm/Views/Cells/ScreenshotCellView.swift
+++ UsabillaFeedbackForm/Views/Cells/ScreenshotCellView.swift
@@ -129,7 +129,7 @@ class ScreenshotCellView: RootCellView {
         super.applyCustomisations()
         deleteIcon.setImage(Icons.imageOfArtboard(color: UIColor.white), for: UIControlState())
         editIcon.setImage(Icons.imageOfEdit(color: UIColor.white), for: UIControlState())
-        addIcon.setImage(Icons.imageOfAddImage(color: UIColor.black), for: UIControlState())
+        addIcon.setImage(Icons.imageOfAddImage(color: themeConfig.textColor), for: UIControlState())
         addScreenshotLabel.setTitleColor(themeConfig.titleColor, for: .normal)
         addScreenshotLabel.titleLabel?.font = themeConfig.font.withSize(themeConfig.titleFontSize)
         addScreenshotLine.backgroundColor = themeConfig.hintColor
