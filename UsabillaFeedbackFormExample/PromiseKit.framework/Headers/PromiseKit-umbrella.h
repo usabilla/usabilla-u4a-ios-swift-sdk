34ab63e76b8c575ab5892cdd9ebbba3030bf418c
Benjamin Grima <grima.benjamin@gmail.com>
2017-01-07 22:09:07 +0100

    Fix choice cell issue where picker was not preselecting the model value

---
+++ UsabillaFeedbackFormExample/PromiseKit.framework/Headers/PromiseKit-umbrella.h
@@ -0,0 +1,14 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "AAA-CocoaPods-Hack.h"
+#import "AnyPromise.h"
+#import "PromiseKit.h"
+#import "PMKUIKit.h"
+#import "UIView+AnyPromise.h"
+#import "UIViewController+AnyPromise.h"
+
+FOUNDATION_EXPORT double PromiseKitVersionNumber;
+FOUNDATION_EXPORT const unsigned char PromiseKitVersionString[];
+
