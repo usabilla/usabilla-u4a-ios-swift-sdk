6697294cc52d3b0c4e522985aa81fc1a34639717
Benjamin Grima <benjamin@usabilla.com>
2017-01-11 14:50:01 +0100

    Create new UI test Project

---
+++ UsabillaFeedbackFormUITestApp/UsabillaFeedbackFormUITestApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UsabillaFeedbackFormUITestApp
+//
+//  Created by Benjamin Grima on 11/01/2017.
+//  Copyright © 2017 Usabilla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
