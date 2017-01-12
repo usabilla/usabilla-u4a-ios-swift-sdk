9c3533a606c1b3701cb9553aac82ec8ce6e17bae
Benjamin Grima <benjamin@usabilla.com>
2017-01-12 12:12:15 +0100

    Update mood control to align left + reverse storyboard

--- UsabillaFeedbackForm/Views/Components/MoodControl.swift
+++ UsabillaFeedbackForm/Views/Components/MoodControl.swift
@@ -13,6 +13,9 @@ class MoodControl: UIControl {
     private let contentView = UIStackView()
     private var selectedIndex = -1
     private let numberOfMood = 5
+    private let moodSize: CGFloat = 60
+    private let spacing: CGFloat = 23
+
     var selectedMood: Int? {
         get {
             return selectedIndex > -1 ? selectedIndex + 1: nil
@@ -51,12 +54,13 @@ class MoodControl: UIControl {
         contentView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
         contentView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
         contentView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        contentView.heightAnchor.constraint(equalToConstant: moodSize).isActive = true
         contentView.distribution = .fillEqually
+
         if !centered {
             contentView.distribution = .fill
-//            contentView.spacing = 23
+            contentView.spacing = spacing
         }
-        contentView.heightAnchor.constraint(equalToConstant: 60).isActive = true
 
         reload()
 
@@ -70,26 +74,34 @@ class MoodControl: UIControl {
 
         let disabled = theme?.disabledEmoticons
 
+        var prevbutton: UIButton?
+
         for i in 0..<numberOfMood {
             let button = UIButton()
             let disabledImage = disabled != nil ? disabled![i] : enabled[i].alpha(value: 0.4)
+            button.imageView?.contentMode = .scaleAspectFit
             button.setImage(disabledImage, for: .normal)
             button.setImage(enabled[i], for: .selected)
             button.addTarget(self, action: #selector(MoodControl.didTouchUpInsideSmiley(_:)), for: .touchUpInside)
-            if !centered {
-                button.setContentHuggingPriority(1000, for: .horizontal)
-            }
+
             contentView.addArrangedSubview(button)
+
+            if !centered {
+                button.setContentHuggingPriority(999, for: .horizontal)
+                if let prev = prevbutton {
+                    button.widthAnchor.constraint(equalTo: prev.widthAnchor).isActive = true
+                } else {
+                    prevbutton = button
+                }
+            }
         }
-        
-        if !centered {
+
+        if !centered { // add blank view to fill stackview
             let stretchingView = UIView()
             stretchingView.setContentHuggingPriority(1, for: .horizontal)
             stretchingView.backgroundColor = .clear
-            stretchingView.translatesAutoresizingMaskIntoConstraints = false
             contentView.addArrangedSubview(stretchingView)
         }
-        
         refreshSelected()
     }
 
