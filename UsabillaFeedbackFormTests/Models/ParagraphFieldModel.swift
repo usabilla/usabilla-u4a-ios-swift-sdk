874444a79419f0e9baf440f13d88539d60309e97
Benjamin Grima <benjamin@usabilla.com>
2017-01-13 14:33:59 +0100

    Add unit test for ParagraphModel

---
+++ UsabillaFeedbackFormTests/Models/ParagraphFieldModel.swift
@@ -0,0 +1,32 @@
+//
+//  ParagraphFieldModelTest.swift
+//  UsabillaFeedbackForm
+//
+//  Created by Benjamin Grima on 13/01/2017.
+//  Copyright © 2017 Usabilla. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+@testable import UsabillaFeedbackForm
+
+class ParagraphFieldModelTest: QuickSpec {
+
+    override func spec() {
+        let pageModel = PageModel(pageNumber: 0, pageName: "test", themeConfig: UsabillaThemeConfigurator())
+        var model: BaseFieldModel?
+
+        describe("ParagraphModel") {
+            it("init ParagraphModel") {
+                model = ParagraphFieldModel(json: JSON.parse("{\"title\":\"test\"}"), pageModel: pageModel)
+                expect(model).toNot(beNil())
+            }
+
+            it("BaseFieldModel convertToJson") {
+                let res = model?.convertToJSON()
+                expect(res).to(beNil())
+            }
+        }
+    }
+}
