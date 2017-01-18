bf1fafc625813e4fca39d783f5b4cff9fd56e56c
Benjamin Grima <benjamin@usabilla.com>
2017-01-18 11:09:13 +0100

    🚨 Add unit tests for Promise

---
+++ UsabillaFeedbackFormTests/Utils/PromiseTest.swift
@@ -0,0 +1,55 @@
+//
+//  PromiseTest.swift
+//  UsabillaFeedbackForm
+//
+//  Created by Benjamin Grima on 18/01/2017.
+//  Copyright © 2017 Usabilla. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+@testable import UsabillaFeedbackForm
+
+class PromiseTest: QuickSpec {
+
+    override func spec() {
+
+        describe("Promise") {
+            it("Promise succeed") {
+                waitUntil(timeout: 2.0) { done in
+                    Promise<Bool> { fullfilled, _ in
+                        DispatchQueue.main.asyncAfter(deadline: DispatchTime(uptimeNanoseconds: 100)) {
+                            fullfilled(true)
+                        }
+                    }.then { success in
+                        expect(success).to(beTrue())
+                        done()
+                    }.catch { _ in
+                        fail("should not go here")
+
+                    }
+
+                }
+            }
+
+            it("Promise failure") {
+                waitUntil(timeout: 2.0) { done in
+                    Promise<Bool> { _, reject in
+                        DispatchQueue.main.asyncAfter(deadline: DispatchTime(uptimeNanoseconds: 100)) {
+                            reject(NSError(domain: "", code: 0, userInfo: nil))
+                        }
+                    }.then { _ in
+                        fail("should not go here")
+
+                    }.catch { err in
+                        expect(err).toNot(beNil())
+                        done()
+                    }
+
+                }
+            }
+
+        }
+    }
+}
