b4cfe574996a2760feb2b366c0108ed1d8d0ac2f
Benjamin Grima <benjamin@usabilla.com>
2017-01-25 14:35:43 +0100

    🚨 Add unit tests for UIFont extension

--- UsabillaFeedbackForm/Utils/Extensions/UIFont.swift
+++ UsabillaFeedbackForm/Utils/Extensions/UIFont.swift
@@ -14,9 +14,8 @@ extension UIFont {
         let descriptor = self.fontDescriptor
         if let derp = descriptor.withSymbolicTraits(UIFontDescriptorSymbolicTraits(traits)) {
             return UIFont(descriptor: derp, size: 0)
-        } else {
-            return nil
         }
+        return nil
     }
     
     func boldItalic() -> UIFont? {
@@ -30,32 +29,5 @@ extension UIFont {
     func bold() -> UIFont? {
         return withTraits(.traitBold)
     }
-    
-    static func registerFontWithFilenameString(_ filenameString: String, bundleIdentifierString: String) {
-        if let bundle = Bundle(identifier: bundleIdentifierString) {
-            if let pathForResourceString = bundle.path(forResource: filenameString, ofType: nil) {
-                if let fontData = try? Data(contentsOf: URL(fileURLWithPath: pathForResourceString)) {
-                    if let dataProvider = CGDataProvider(data: fontData as CFData) {
-                        let fontRef = CGFont(dataProvider)
-                        var errorRef: Unmanaged<CFError>? = nil
-                        if (CTFontManagerRegisterGraphicsFont(fontRef, &errorRef) == false) {
-                            NSLog("UIFont+:  Failed to register font - register graphics font failed - this font may have already been registered in the main bundle.")
-                            
-                        } else {
-                            NSLog("UIFont+:  Failed to register font - font could not be loaded.")
-                        }
-                    } else {
-                        NSLog("UIFont+:  Failed to register font - data provider could not be loaded.")
-                    }
-                } else {
-                    NSLog("UIFont+:  Failed to register font - font data could not be loaded.")
-                }
-            } else {
-                NSLog("UIFont+:  Failed to register font - path for resource not found.")
-            }
-        } else {
-            NSLog("UIFont+:  Failed to register font - bundle identifier invalid.")
-        }
-    }
-    
+
 }
