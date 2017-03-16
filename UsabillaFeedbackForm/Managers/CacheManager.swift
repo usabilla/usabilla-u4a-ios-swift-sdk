25096dcfc08083d9e7a467a86ffe96321302fd62
Adil Bougamza <adil.bougamza@gmail.com>
2017-03-16 11:59:58 +0100

    Add remove form from cache in CacheManager

--- UsabillaFeedbackForm/Managers/CacheManager.swift
+++ UsabillaFeedbackForm/Managers/CacheManager.swift
@@ -18,13 +18,11 @@ class CacheManager {
     }
     
     // MARK: Form Caching Methods
-    func cacheForm(id: String, form: FormModel) -> Bool
-    {
+    func cacheForm(id: String, form: FormModel) -> Bool {
         return saveToCacheForm(id: id, formJson: form.formJsonString)
     }
     
-    func getForm(id: String) -> FormModel?
-    {
+    func getForm(id: String) -> FormModel? {
         if let jsonForm = getJsonForm(id: id) {
             return JSONFormParser.parseFormJson(jsonForm, appId: id, screenshot: nil, themeConfig: UsabillaThemeConfigurator())
         }
@@ -32,11 +30,14 @@ class CacheManager {
         return nil
     }
     
+    func removeCachedForm(id: String) {
+        removeFormId(id: id)
+    }
+    
     // MARK: Dictionary caching mechanism
-    private func getJsonForm(id: String) -> JSON?
-    {
+    private func getJsonForm(id: String) -> JSON? {
         print("[CacheManager] : getting \(id) from cache")
-        let fileName = "\(filePrefix)-\(id)"
+        let fileName = fileNameFormatterForForm(id: id)
         do {
             let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
             let fileURL = documentsURL.appendingPathComponent("\(fileName)")
@@ -48,22 +49,41 @@ class CacheManager {
         } catch {
             Swift.debugPrint("[CacheManager] : getting \(id) from cache -> Error ❌")
         }
+        
         return nil
     }
     
-    private func saveToCacheForm(id: String, formJson: JSON) -> Bool
-    {
-        let fileName = "\(filePrefix)-\(id)"
+    private func saveToCacheForm(id: String, formJson: JSON) -> Bool {
+        let fileName = fileNameFormatterForForm(id: id)
         do {
             let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
             let fileURL = documentsURL.appendingPathComponent("\(fileName)")
             let data = try formJson.rawData()
             try data.write(to: fileURL)
             Swift.debugPrint("[CacheManager] : saving \(id) to cache -> OK ✅")
+            
             return true
         } catch {
             Swift.debugPrint("[CacheManager] : saving \(id) to cache -> Error ❌")
             return false
         }
+    }
+    
+    private func removeFormId(id: String) {
+        let fileName = fileNameFormatterForForm(id: id)
+        
+        let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let fileURL = documentsURL.appendingPathComponent("\(fileName)")
+        
+        do {
+            try FileManager.default.removeItem(at: fileURL)
+            Swift.debugPrint("file deleted ✅")
+        } catch {
+            Swift.debugPrint("file not found ❌")
+        }
+    }
+    
+    private func fileNameFormatterForForm(id: String) -> String {
+        return "\(filePrefix)-\(id)"
     }
 }
