626b1b3116544e1593f6bb6827fac39a37338c49
Giacomo Pinato <giacomo@usabilla.com>
2017-03-29 11:19:01 +0200

    adds tests

---
+++ UsabillaFeedbackFormTests/EventEngineTests/RulesTests/AndRuleTest.swift
@@ -0,0 +1,82 @@
+//
+//  AndRuleTest.swift
+//  UsabillaFeedbackForm
+//
+//  Created by Giacomo Pinato on 28/03/2017.
+//  Copyright © 2017 Usabilla. All rights reserved.
+//
+
+
+import Quick
+import Nimble
+
+@testable import UsabillaFeedbackForm
+
+class AndRuleTest: QuickSpec {
+
+    let event1 = Event(name: "event1")
+    let event2 = Event(name: "event2")
+    let event3 = Event(name: "event3")
+    let event4 = Event(name: "event4")
+
+    var leafRule1: Rule!
+    var leafRule2: Rule!
+    var leafRule3: Rule!
+    var leafRule4: Rule!
+
+    var allPositive: [Rule]!
+    var allNegative: [Rule]!
+    var mixed: [Rule]!
+
+
+    override func spec() {
+        
+        beforeEach {
+            self.leafRule1 = LeafRule(event: self.event1, ruleID: "id1", alreadyTriggered: true)
+            self.leafRule2 = LeafRule(event: self.event2, ruleID: "id2", alreadyTriggered: true)
+            self.leafRule3 = LeafRule(event: self.event3, ruleID: "id3", alreadyTriggered: false)
+            self.leafRule4 = LeafRule(event: self.event4, ruleID: "id4", alreadyTriggered: false)
+            
+            
+            self.allPositive = [self.leafRule1, self.leafRule2]
+            self.allNegative = [self.leafRule3, self.leafRule4]
+            self.mixed = [self.leafRule1, self.leafRule2, self.leafRule3]
+        }
+        
+        
+        describe("And Rule") {
+
+            context("When creating an object", {
+                it("should initialise correctly") {
+                    let newAnd = AndRule(childRules: [self.leafRule1])
+                    expect(newAnd.type).to(equal(RuleType.and))
+                    expect(newAnd.alreadyTriggered).to(beFalse())
+
+                }
+            })
+            
+            
+            
+            context("when checking for validity", {
+                it("should return true if all children as satisfied") {
+                    let newAnd = AndRule(childRules: self.allPositive)
+                    expect(newAnd.triggersWith(event: self.event1)).to(beTrue())
+                    expect(newAnd.triggersWith(event: self.event4)).to(beTrue())
+                    
+                    let newAnd2 = AndRule(childRules: self.mixed)
+                    expect(newAnd2.triggersWith(event: self.event3)).to(beTrue())
+                }
+                
+                it("should return false if even one children is not satisfied") {
+                               
+                    let newAnd2 = AndRule(childRules: self.mixed)
+                    expect(newAnd2.triggersWith(event: self.event2)).to(beFalse())
+                }
+            })
+            
+            
+            
+            
+        }
+    }
+}
---
+++ UsabillaFeedbackFormTests/EventEngineTests/RulesTests/ConcreteRuleTest.swift
@@ -0,0 +1,87 @@
+//
+//  ConcreteRuleTest.swift
+//  UsabillaFeedbackForm
+//
+//  Created by Giacomo Pinato on 28/03/2017.
+//  Copyright © 2017 Usabilla. All rights reserved.
+//
+
+
+import Quick
+import Nimble
+
+@testable import UsabillaFeedbackForm
+
+class ConcreteRuleTest: QuickSpec {
+
+    let event1 = Event(name: "event1")
+    let event2 = Event(name: "event2")
+    let event3 = Event(name: "event3")
+    let event4 = Event(name: "event4")
+
+    var leafRule1: Rule!
+    var leafRule2: Rule!
+    var leafRule3: Rule!
+    var leafRule4: Rule!
+
+    var allPositive: [Rule]!
+    var allNegative: [Rule]!
+    var mixed: [Rule]!
+
+
+    override func spec() {
+
+        beforeEach {
+            self.leafRule1 = LeafRule(event: self.event1, ruleID: "id1", alreadyTriggered: true)
+            self.leafRule2 = LeafRule(event: self.event2, ruleID: "id2", alreadyTriggered: true)
+            self.leafRule3 = LeafRule(event: self.event3, ruleID: "id3", alreadyTriggered: false)
+            self.leafRule4 = LeafRule(event: self.event4, ruleID: "id4", alreadyTriggered: false)
+
+
+            self.allPositive = [self.leafRule1, self.leafRule2]
+            self.allNegative = [self.leafRule3, self.leafRule4]
+            self.mixed = [self.leafRule1, self.leafRule2, self.leafRule3]
+        }
+
+
+        describe("Concrete Rule") {
+
+            context("The object", {
+                it("should initialise correctly") {
+                    let or = ConcreteRule(type: .or, childRules: self.mixed)
+                    expect(or.type).to(equal(RuleType.or))
+                    expect(or.ruleID).toNot(beNil())
+                    expect(or.alreadyTriggered).to(beFalse())
+
+
+                    let and = ConcreteRule(type: .and, childRules: self.mixed, ruleID: "id", alreadyTriggered: true)
+                    expect(and.type).to(equal(RuleType.and))
+                    expect(and.ruleID).to(equal("id"))
+                    expect(and.alreadyTriggered).to(beTrue())
+
+                }
+
+
+
+                it("should serialise correctly") {
+                    let or = ConcreteRule(type: .and, childRules: self.mixed, ruleID: "id", alreadyTriggered: true)
+                    let data = NSKeyedArchiver.archivedData(withRootObject: or)
+
+                    expect(data).toNot(beNil())
+                    // swiftlint:disable force_cast
+                    let unserialised = NSKeyedUnarchiver.unarchiveObject(with: data) as! ConcreteRule
+
+                    expect(unserialised).toNot(beNil())
+                    expect(unserialised.ruleID).to(equal("id"))
+                    expect(unserialised.type).to(equal(RuleType.and))
+                    expect(unserialised.type).toNot(equal(RuleType.or))
+
+                }
+
+
+            })
+
+
+        }
+    }
+}
