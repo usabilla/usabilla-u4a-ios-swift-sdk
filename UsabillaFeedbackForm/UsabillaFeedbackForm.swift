8baf2981905123062cc9c3254b89607e1c68f315
Giacomo Pinato <giacomo@usabilla.com>
2017-04-05 15:52:31 +0200

    Add method to public interface

--- UsabillaFeedbackForm/UsabillaFeedbackForm.swift
+++ UsabillaFeedbackForm/UsabillaFeedbackForm.swift
@@ -18,11 +18,17 @@ open class UsabillaFeedbackForm {
     open static var showCancelButton: Bool = false
     open static var dismissAutomatically: Bool = true
     open static var theme: UsabillaTheme = UsabillaTheme()
+    open static var canDisplayCampaign: Bool = true
+
     static var defaultLocalisationFile = true
     open static var localizedStringFile: String = "usa_localizable" {
         didSet {
             defaultLocalisationFile = false
         }
+    }
+
+    open class func sendEvent(event: String) {
+        CampaignsManager.sendEvent(event: event)
     }
 
     /**
@@ -34,6 +40,7 @@ open class UsabillaFeedbackForm {
     */
     open class func load() {
         _ = SubmissionManager.shared // init the singleton to send persisted feedback
+        CampaignsManager.start() // init the campaigns manager, fetching the latest version
     }
 
     open class func removeCachedForms() {
