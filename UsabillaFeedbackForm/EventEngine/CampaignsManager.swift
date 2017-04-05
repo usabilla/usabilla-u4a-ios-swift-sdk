0bb51d1cc5005a2db2c59e35a6c9694d622ed219
Giacomo Pinato <giacomo@usabilla.com>
2017-04-05 15:52:43 +0200

    Add mock classes

---
+++ UsabillaFeedbackForm/EventEngine/CampaignsManager.swift
@@ -0,0 +1,31 @@
+//
+//  CampaignManager.swift
+//  UsabillaFeedbackForm
+//
+//  Created by Giacomo Pinato on 05/04/2017.
+//  Copyright © 2017 Usabilla. All rights reserved.
+//
+
+import Foundation
+
+class CampaignsManager {
+
+    static func sendEvent(event: String) {}
+
+    class func start() {
+        //Fetch campaigns from BE
+    }
+
+    class func saveCampaignsToDB() {
+        //Save the status of all campaigns
+    }
+
+    class func saveCampaignToDB(campaign: CampaignModel) {
+        //Saves a single campaign in DB
+    }
+
+    class func fetchCampaigns() -> [CampaignModel] {
+        //Retrieves campaigns from DB
+    }
+
+}
---
+++ UsabillaFeedbackForm/EventEngine/EventEngine.swift
@@ -0,0 +1,13 @@
+//
+//  EventEngine.swift
+//  UsabillaFeedbackForm
+//
+//  Created by Giacomo Pinato on 05/04/2017.
+//  Copyright © 2017 Usabilla. All rights reserved.
+//
+
+import Foundation
+
+class EventEngine {
+
+}
