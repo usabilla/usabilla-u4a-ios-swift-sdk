1dd1a0eedf9f1e5c712ed923e0847a5d2cfe09f7
Adil Bougamza <adil.bougamza@gmail.com>
2017-04-10 10:53:05 +0200

    create UBFormDAO + formModel conform to NSCoding

---
+++ UsabillaFeedbackForm/DAO/UBFormDAO.swift
@@ -0,0 +1,13 @@
+//
+//  UBFormDAO.swift
+//  UsabillaFeedbackForm
+//
+//  Created by Adil Bougamza on 07/04/2017.
+//  Copyright © 2017 Usabilla. All rights reserved.
+//
+
+import Foundation
+
+class UBFormDAO: UBFileStorageDAO<FormModel> {
+    
+}
