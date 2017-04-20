1e4d3ea7f9341c809c84de49440c4ecf33cb4af3
Giacomo Pinato <giacomo@usabilla.com>
2017-04-20 10:53:48 +0200

    Adds request builder, changes api endpoint in user settings to add http:// in front

--- UsabillaFeedbackForm/Network/RequestBuilder.swift
+++ UsabillaFeedbackForm/Network/RequestBuilder.swift
@@ -6,12 +6,17 @@
 //  Copyright © 2017 Usabilla. All rights reserved.
 //
 
+// swiftlint:disable force_cast
+
 class RequestBuilder {
 
+    enum Endpoints: String {
+        case campaignForm = "/v2/sdk/forms/"
+    }
+
     static let bundle = Bundle(for: NetworkManager.self)
-    static let apiUrl = bundle.infoDictionary!["USABILLA_API_URL"] as? String ?? ""
-    static let submitUrl = bundle.infoDictionary!["USABILLA_SUBMIT_ENDPOINT"] as? String ?? ""
-    //static let getCampaignsUrl = bundle.infoDictionary!["USABILLA_SUBMIT_ENDPOINT"] as? String ?? ""
+    static let apiUrl: String = bundle.infoDictionary!["USABILLA_API_URL"] as! String
+    static let submitUrl: String = bundle.infoDictionary!["USABILLA_SUBMIT_ENDPOINT"] as! String
 
     static let headers: [String: String]? = [
         "app-version": Bundle.main.infoDictionary!["CFBundleVersion"] as? String ?? "",
@@ -20,19 +25,65 @@ class RequestBuilder {
         "os": "iOS"
     ]
 
-    private class func buildBaseGet(withURL url: URL, withHTTPMethod method: HTTPMethod) -> NSMutableURLRequest {
+    /**
+     Creates a URL
+     
+     @param endpoint: the endpoint of the URL
+     
+     @param param: parameter to add at the end of the URL
+     
+     @return and URL with endpoint and parameter
+     */
+    private class func buildURL(withEndpoint endpoint: Endpoints, withURLParam param: String?) -> URL {
+        if let param = param {
+            return URL(string: apiUrl.appending(endpoint.rawValue).appending(param))!
+        }
+        return URL(string: apiUrl.appending(endpoint.rawValue))!
+    }
+
+    /**
+     Creates a basic GET request
+     
+     @param url: the url to use for the GET request
+     
+     @return the basic GET request
+     */
+    private class func buildBaseGet(withURL url: URL) -> NSMutableURLRequest {
         let request: NSMutableURLRequest = NSMutableURLRequest(url: url)
         request.cachePolicy = .useProtocolCachePolicy
-        request.httpMethod = method.rawValue
+        request.httpMethod = HTTPMethod.get.rawValue
         request.allHTTPHeaderFields = headers
 
         return request
     }
 
-    class func buildGetAllCampaigns() -> NSMutableURLRequest {
-        let url = URL(string: apiUrl)!
-        let urlRequest = buildBaseGet(withURL: url, withHTTPMethod: HTTPMethod.get)
-        return urlRequest
+    /**
+     Creates a basic POST request
+     
+     @param url: the url to use for the POST request
+     
+     @param parameters: the parameters to use for the POST
+
+     @return the basic POST request
+     */
+    private class func buildBasePost(withURL url: URL, parameters: Parameters) -> NSMutableURLRequest {
+        var request: NSMutableURLRequest = NSMutableURLRequest(url: url)
+        request.cachePolicy = .useProtocolCachePolicy
+        request.httpMethod = HTTPMethod.post.rawValue
+        request.allHTTPHeaderFields = headers
+
+        do {
+            request = try JSONEncoding.default.encode(request, with: parameters)
+        } catch {
+            //Do intelligent stuff with error
+        }
+
+        return request
+    }
+
+    class func buildGetCampaignForm(withFormId id: String) -> NSURLRequest {
+        let url = buildURL(withEndpoint: .campaignForm, withURLParam: id)
+        return buildBaseGet(withURL: url)
     }
 
 }
