35a8383c579df55692327153f946b745f9086da2
Giacomo Pinato <giacomo@usabilla.com>
2017-04-20 11:54:29 +0200

    add campaignService

---
+++ UsabillaFeedbackForm/Network/Services/CampaignService.swift
@@ -0,0 +1,23 @@
+//
+//  CampaignService.swift
+//  UsabillaFeedbackForm
+//
+//  Created by Giacomo Pinato on 18/04/2017.
+//  Copyright © 2017 Usabilla. All rights reserved.
+//
+
+class CampaignService {
+
+
+    class func getCampaignForm(withId id: String) -> Promise<FormModel> {
+        let request = RequestBuilder.buildGetCampaignForm(withFormId: id)
+        return Promise { fulfill, reject in
+            HTTPClient.request(request: request as URLRequest, completion: { response in
+                if let json = response.data {
+                    fulfill(FormModel(json: json, id: "a", screenshot: nil))
+                }
+                reject(response.error!)
+            })
+        }
+    }
+}
