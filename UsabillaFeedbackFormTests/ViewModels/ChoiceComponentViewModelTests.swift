af12b6c0692963d4c41848e225d9fb19a9505def
Giacomo Pinato <Jack@Giacomos-MacBook-Pro.local>
2017-04-24 14:49:35 +0200

    🚨add unit test

--- UsabillaFeedbackFormTests/ViewModels/ChoiceComponentViewModelTests.swift
+++ UsabillaFeedbackFormTests/ViewModels/ChoiceComponentViewModelTests.swift
@@ -1,5 +1,5 @@
 //
-//  ChoiceComponentViewModelTests.swift
+//  ChoiceComponentviewModelTests.swift
 //  UsabillaFeedbackForm
 //
 //  Created by Benjamin Grima on 20/03/2017.
@@ -15,42 +15,102 @@ class ChoiceComponentViewModelTests: QuickSpec {
 
     override func spec() {
 
+        var viewModelEmpty: ChoiceComponentViewModel!
+        var modelEmpty: ChoiceFieldModel!
+
+        var viewModelDefault: ChoiceComponentViewModel!
+        var modelDefault: ChoiceFieldModel!
+
         var viewModel: ChoiceComponentViewModel!
         var model: ChoiceFieldModel!
+
         let pageModel = PageModel(pageNumber: 0, pageName: "")
 
-        beforeSuite {
-            let path = Bundle(for: JSONParserTest.self).path(forResource: "test", ofType: "json")!
-            let data = try NSData(contentsOf: NSURL(fileURLWithPath: path) as URL, options: NSData.ReadingOptions.mappedIfSafe)!
-            let jsonObj: JSON = JSON(data: data as Data)
+        beforeEach {
+            var path = Bundle(for: ChoiceComponentViewModelTests.self).path(forResource: "PickerWithEmpty", ofType: "json")!
+            var data = try? NSData(contentsOf: NSURL(fileURLWithPath: path) as URL, options: NSData.ReadingOptions.mappedIfSafe)
+            var jsonObj: JSON = JSON(data: (data as Data?)!)
+            modelEmpty = ChoiceFieldModel(json: jsonObj, pageModel: pageModel)
+            viewModelEmpty = ChoiceComponentViewModel(model: modelEmpty, theme: UsabillaTheme())
 
+            path = Bundle(for: ChoiceComponentViewModelTests.self).path(forResource: "PickerWithDefault", ofType: "json")!
+            data = try? NSData(contentsOf: NSURL(fileURLWithPath: path) as URL, options: NSData.ReadingOptions.mappedIfSafe)
+            jsonObj = JSON(data: (data as Data?)!)
+            modelDefault = ChoiceFieldModel(json: jsonObj, pageModel: pageModel)
+            viewModelDefault = ChoiceComponentViewModel(model: modelDefault, theme: UsabillaTheme())
+
+            path = Bundle(for: ChoiceComponentViewModelTests.self).path(forResource: "Picker", ofType: "json")!
+            data = try? NSData(contentsOf: NSURL(fileURLWithPath: path) as URL, options: NSData.ReadingOptions.mappedIfSafe)
+            jsonObj = JSON(data: (data as Data?)!)
             model = ChoiceFieldModel(json: jsonObj, pageModel: pageModel)
             viewModel = ChoiceComponentViewModel(model: model, theme: UsabillaTheme())
+
         }
 
-        describe("ChoiceComponentViewModelTests") {
-            it("viewModel options should match model options") {
-                expect(model.options.count).to(equal(viewModel.options.count))
+        describe("ChoiceComponentviewModelEmptyTests") {
+            it("viewModelEmpty options should match model options") {
+                expect(modelEmpty.options.count).to(equal(viewModelEmpty.options.count))
             }
 
             context("when updating value") {
                 it("should update model") {
-                    viewModel.value = "hello"
-                    expect(model.fieldValue).to(equal(["hello"]))
+                    viewModelEmpty.value = "hello"
+                    expect(modelEmpty.fieldValue).to(equal(["hello"]))
                 }
 
-                it("should update viewModel") {
-                    viewModel.value = "hi"
-                    expect(viewModel.value).to(equal("hi"))
+                it("should update viewModelEmpty") {
+                    viewModelEmpty.value = "hi"
+                    expect(viewModelEmpty.value).to(equal("hi"))
                 }
             }
 
-            context("when retrieving values") {
-                it("should return the correct picket button title") {
-                    viewModel.value = "hello"
-                    expect(viewModel.pickerButtonTitle).to(equal("hello"))
+            context("when retrieving the picker Button") {
+                it("should return the correct picket button title when a value is selected") {
+                    modelEmpty.fieldValue = ["Option_1"]
+                    modelDefault.fieldValue = ["Option_1"]
+                    model.fieldValue = ["Option_1"]
+                    expect(viewModelDefault.pickerButtonTitle).to(equal("Option 1"))
+                    expect(viewModel.pickerButtonTitle).to(equal("Option 1"))
+                    expect(viewModelEmpty.pickerButtonTitle).to(equal("Option 1"))
                 }
+                it("should return the correct picket button title with no value selected and empty button") {
+                    modelEmpty.fieldValue = []
+                    expect(viewModelEmpty.pickerButtonTitle).to(equal("Empty"))
+                }
+                it("should return the correct picket button title with no value selected and no empty button") {
+                    modelDefault.fieldValue = []
+                    model.fieldValue = []
+                    expect(viewModelDefault.pickerButtonTitle).to(beNil())
+                    expect(viewModel.pickerButtonTitle).to(beNil())
+                }
+                it("should return default value if there is one") {
+                    expect(viewModelDefault.value).to(equal("Option_3"))
+                    expect(modelDefault.fieldValue).to(equal(["Option_3"]))
+                    expect(viewModelDefault.pickerButtonTitle).to(equal("Option 3"))
+                    expect(viewModel.pickerButtonTitle).to(beNil())
+                }
+            }
 
+            context("when retrieving the index of the selected element") {
+                it("should return nil when no element is selected") {
+                    modelEmpty.fieldValue = []
+                    modelDefault.fieldValue = []
+                    model.fieldValue = []
+                    expect(viewModelDefault.indexOfSelectedOption).to(beNil())
+                    expect(viewModel.indexOfSelectedOption).to(beNil())
+                    expect(viewModelEmpty.indexOfSelectedOption).to(beNil())
+                }
+                it("should return the correct index for the default value") {
+                    expect(viewModelDefault.indexOfSelectedOption).to(equal(2))
+                }
+                it("should return the correct index  when a value is selected") {
+                    modelEmpty.fieldValue = ["Option_1"]
+                    modelDefault.fieldValue = ["Option_1"]
+                    model.fieldValue = ["Option_1"]
+                    expect(viewModelDefault.indexOfSelectedOption).to(equal(0))
+                    expect(viewModel.indexOfSelectedOption).to(equal(0))
+                    expect(viewModelEmpty.indexOfSelectedOption).to(equal(0))
+                }
             }
         }
     }
