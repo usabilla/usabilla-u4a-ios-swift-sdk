2b842c59383d449656dc039ba7ccb45e545ea438
Benjamin Grima <benjamin@usabilla.com>
2017-04-25 13:43:16 +0200

    🚨 Add unit tests for campaign manager

--- UsabillaFeedbackFormTests/EventEngineTests/CampaignManagerTests.swift
+++ UsabillaFeedbackFormTests/EventEngineTests/CampaignManagerTests.swift
@@ -68,6 +68,9 @@ class CampaignManagerTests: QuickSpec {
                 beforeEach {
                     storeMock.campaigns = []
                     storeMock.onSaveCalled = nil
+                    UBCampaignDAO.shared.deleteAll()
+                    CampaignWindow.shared.campaignDidEnd(success: false)
+                    UsabillaFeedbackForm.canDisplayCampaigns = true
                 }
 
                 it("should not save campaigns when there are no responding campaigns") {
@@ -84,10 +87,6 @@ class CampaignManagerTests: QuickSpec {
                     storeMock.campaigns = campaigns
 
                     let campaignManager = CampaignManager(campaignStore: storeMock, appId: "test")
-
-                    storeMock.onSaveCalled = { _ in
-                        fail("should not be called")
-                    }
                     campaignManager.sendEvent(event: "foo")
                 }
 
@@ -97,21 +96,107 @@ class CampaignManagerTests: QuickSpec {
                     let leaf2 = LeafRule(event: Event(name: "bar"))
                     let rule = AndRule(childRules: [leaf, leaf2])
 
-                    let campaigns = [
-                        CampaignModel(id: "a", rule: rule, formId: "", maximumDisplays: 0, version: 0),
-                        CampaignModel(id: "b", json: JSON.parse(""))
-                    ]
+                    UBCampaignDAO.shared.create(CampaignModel(id: "a", rule: rule, formId: "", maximumDisplays: 0, version: 0))
+                    UBCampaignDAO.shared.create(CampaignModel(id: "b", json: JSON.parse("")))
 
-                    storeMock.campaigns = campaigns
+                    let campaignManager = CampaignManager(campaignStore: UBCampaignStore(), appId: "test")
+                    var triggeredRule = UBCampaignDAO.shared.read(id: "a")?.rule?.childRules.first as? LeafRule
+                    expect(triggeredRule?.event.name).to(equal("foo"))
+                    expect(triggeredRule?.alreadyTriggered).to(beFalse())
+                    campaignManager.sendEvent(event: "foo")
+                    triggeredRule = UBCampaignDAO.shared.read(id: "a")?.rule?.childRules.first as? LeafRule
+                    expect(triggeredRule?.event.name).to(equal("foo"))
+                    expect(triggeredRule?.alreadyTriggered).to(beTrue())
+                }
 
-                    let campaignManager = CampaignManager(campaignStore: storeMock, appId: "test")
+                it("should not display campaign that have not triggered") {
 
-                    waitUntil(timeout: 2.0) { done in
-                        storeMock.onSaveCalled = { _ in
-                            done()
-                        }
-                        campaignManager.sendEvent(event: "foo")
-                    }
+                    let leaf = LeafRule(event: Event(name: "foo"))
+                    let leaf2 = LeafRule(event: Event(name: "bar"))
+                    let rule = AndRule(childRules: [leaf, leaf2])
+
+                    UBCampaignDAO.shared.create(CampaignModel(id: "a", rule: rule, formId: "", maximumDisplays: 0, version: 0))
+                    UBCampaignDAO.shared.create(CampaignModel(id: "b", json: JSON.parse("")))
+
+                    let campaignManager = CampaignManager(campaignStore: UBCampaignStore(), appId: "test")
+                    var campaign = UBCampaignDAO.shared.read(id: "a")
+                    expect(campaign?.numberOfTimesTriggered).to(equal(0))
+                    campaignManager.sendEvent(event: "foo")
+                    campaign = UBCampaignDAO.shared.read(id: "a")
+                    expect(campaign?.numberOfTimesTriggered).to(equal(0))
+                }
+
+                it("should display campaign that triggered") {
+
+                    let leaf = LeafRule(event: Event(name: "foo"))
+                    let leaf2 = LeafRule(event: Event(name: "bar"))
+                    let rule = AndRule(childRules: [leaf, leaf2])
+
+                    UBCampaignDAO.shared.create(CampaignModel(id: "a", rule: rule, formId: "", maximumDisplays: 0, version: 0))
+                    UBCampaignDAO.shared.create(CampaignModel(id: "b", json: JSON.parse("")))
+
+                    let campaignManager = CampaignManager(campaignStore: UBCampaignStore(), appId: "test")
+                    var campaign = UBCampaignDAO.shared.read(id: "a")
+                    expect(campaign?.numberOfTimesTriggered).to(equal(0))
+                    campaignManager.sendEvent(event: "foo")
+                    campaign = UBCampaignDAO.shared.read(id: "a")
+                    expect(campaign?.numberOfTimesTriggered).to(equal(0))
+                    campaignManager.sendEvent(event: "bar")
+                    campaign = UBCampaignDAO.shared.read(id: "a")
+                    expect(campaign?.numberOfTimesTriggered).to(equal(1))
+                }
+
+                it("should display only the first campaign that triggered") {
+
+                    let leaf = LeafRule(event: Event(name: "foo"))
+                    let leaf2 = LeafRule(event: Event(name: "bar"))
+                    let rule = AndRule(childRules: [leaf, leaf2])
+
+                    UBCampaignDAO.shared.create(CampaignModel(id: "a", rule: rule, formId: "", maximumDisplays: 0, version: 0))
+                    UBCampaignDAO.shared.create(CampaignModel(id: "b", rule: rule, formId: "", maximumDisplays: 0, version: 0))
+                    let campaignManager = CampaignManager(campaignStore: UBCampaignStore(), appId: "test")
+                    var campaignA = UBCampaignDAO.shared.read(id: "a")
+                    var campaignB = UBCampaignDAO.shared.read(id: "b")
+                    expect(campaignA?.numberOfTimesTriggered).to(equal(0))
+                    expect(campaignB?.numberOfTimesTriggered).to(equal(0))
+                    campaignManager.sendEvent(event: "foo")
+                    campaignA = UBCampaignDAO.shared.read(id: "a")
+                    campaignB = UBCampaignDAO.shared.read(id: "b")
+                    expect(campaignA?.numberOfTimesTriggered).to(equal(0))
+                    expect(campaignB?.numberOfTimesTriggered).to(equal(0))
+                    campaignManager.sendEvent(event: "bar")
+                    campaignA = UBCampaignDAO.shared.read(id: "a")
+                    campaignB = UBCampaignDAO.shared.read(id: "b")
+                    expect(campaignA?.numberOfTimesTriggered).to(equal(1))
+                    expect(campaignB?.numberOfTimesTriggered).to(equal(0))
+                }
+
+                it("should display campaign if the semaphore allow it and the campaign can be displayed again") {
+                    let leaf = LeafRule(event: Event(name: "foo"))
+                    let leaf2 = LeafRule(event: Event(name: "bar"))
+                    let rule = AndRule(childRules: [leaf, leaf2])
+
+                    UBCampaignDAO.shared.create(CampaignModel(id: "a", rule: rule, formId: "", maximumDisplays: 0, version: 0))
+                    let campaignManager = CampaignManager(campaignStore: UBCampaignStore(), appId: "test")
+                    var campaignA = UBCampaignDAO.shared.read(id: "a")
+                    expect(campaignA?.numberOfTimesTriggered).to(equal(0))
+
+                    UsabillaFeedbackForm.canDisplayCampaigns = false
+                    campaignManager.displayCampaign(campaignA!)
+                    campaignA = UBCampaignDAO.shared.read(id: "a")
+                    expect(campaignA?.numberOfTimesTriggered).to(equal(0))
+
+                    UsabillaFeedbackForm.canDisplayCampaigns = true
+                    campaignA?.maximumDisplays = 1
+                    campaignA?.numberOfTimesTriggered = 1
+                    campaignManager.displayCampaign(campaignA!)
+                    campaignA = UBCampaignDAO.shared.read(id: "a")
+                    expect(campaignA?.numberOfTimesTriggered).to(equal(0))
+
+                    campaignA?.numberOfTimesTriggered = 0
+                    campaignManager.displayCampaign(campaignA!)
+                    campaignA = UBCampaignDAO.shared.read(id: "a")
+                    expect(campaignA?.numberOfTimesTriggered).to(equal(1))
                 }
             }
         }
