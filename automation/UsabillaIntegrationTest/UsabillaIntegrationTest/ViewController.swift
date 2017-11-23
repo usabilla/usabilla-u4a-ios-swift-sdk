fb4612ec4e5c63037924834e395505411469973e
Benjamin Grima <benjamin@usabilla.com>
2017-11-23 11:13:10 +0100

    Add Integration test project

---
+++ automation/UsabillaIntegrationTest/UsabillaIntegrationTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UsabillaIntegrationTest
+//
+//  Created by Benjamin Grima on 27/11/2017.
+//  Copyright © 2017 Usabilla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ automation/UsabillaIntegrationTest/UsabillaIntegrationTestUITests/UsabillaIntegrationTestUITests.swift
@@ -0,0 +1,25 @@
+//
+//  UsabillaIntegrationTestUITests.swift
+//  UsabillaIntegrationTestUITests
+//
+//  Created by Benjamin Grima on 27/11/2017.
+//  Copyright © 2017 Usabilla. All rights reserved.
+//
+
+import XCTest
+
+class UsabillaIntegrationTestUITests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        continueAfterFailure = false
+        XCUIApplication().launch()
+
+    }
+
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
