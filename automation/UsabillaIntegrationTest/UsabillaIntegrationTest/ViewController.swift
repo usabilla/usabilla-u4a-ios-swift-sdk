91951740b6e1f8daac81abb361c686d5b2bcfab3
Adil Bougamza <adil.bougamza@gmail.com>
2017-11-28 16:17:49 +0100

    implement ui test to trigger and display a campaign

--- automation/UsabillaIntegrationTest/UsabillaIntegrationTest/ViewController.swift
+++ automation/UsabillaIntegrationTest/UsabillaIntegrationTest/ViewController.swift
@@ -12,17 +12,12 @@ import Usabilla
 class ViewController: UIViewController {
 
     @IBOutlet weak var textField: UITextField!
-    @IBOutlet weak var buttonSend: UIButton!
-    @IBOutlet weak var resetButton: UIButton!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         textField.text = "purchaseComplete"
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        Usabilla.customVariables = ["frequent flyer status": "sky elite"]
     }
 
     @IBAction func sendAction(_ sender: Any) {
--- automation/UsabillaIntegrationTest/UsabillaIntegrationTestUITests/UsabillaIntegrationTestUITests.swift
+++ automation/UsabillaIntegrationTest/UsabillaIntegrationTestUITests/UsabillaIntegrationTestUITests.swift
@@ -12,14 +12,22 @@ class UsabillaIntegrationTestUITests: XCTestCase {
 
     override func setUp() {
         super.setUp()
+        let app = XCUIApplication()
+        app.launchArguments += ["-AppleLanguages", "(nl)"]
+        app.launchArguments += ["-AppleLocale", "nl_NL"]
         continueAfterFailure = false
-        XCUIApplication().launch()
-
+        app.launch()
     }
 
     func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+        let app = XCUIApplication()
+        app.buttons["reset"].tap()
 
+        let sendButton = app.buttons["send"]
+        sendButton.tap()
+        sendButton.tap()
+
+        let ratingcontrolElement = app.otherElements["ratingControl"]
+        XCTAssert(ratingcontrolElement.exists, "rating control banner exists")
+    }
 }
