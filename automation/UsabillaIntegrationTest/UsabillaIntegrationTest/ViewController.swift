8862dcc07601a5263bd6ae0030c55e7bbdcdd70a
Adil Bougamza <adil.bougamza@gmail.com>
2017-11-28 13:29:51 +0100

    📦 create basic UI and link to app

--- automation/UsabillaIntegrationTest/UsabillaIntegrationTest/ViewController.swift
+++ automation/UsabillaIntegrationTest/UsabillaIntegrationTest/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var buttonSend: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sendAction(_ sender: Any) {
+    }
 
 }
 
