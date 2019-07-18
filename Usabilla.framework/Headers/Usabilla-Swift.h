d447435e8f417a5d8cc57c6a132f48d440d41235
Anders Liebl <anders.liebl@usabilla.com>
2019-07-18 14:23:01 +0200

    Release v6.3.0

--- Usabilla.framework/Headers/Usabilla-Swift.h
+++ Usabilla.framework/Headers/Usabilla-Swift.h
@@ -209,6 +209,8 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 
 
 
+
+
 #if __has_attribute(external_source_symbol)
 # pragma clang attribute pop
 #endif
@@ -401,6 +403,8 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 # pragma clang attribute push(__attribute__((external_source_symbol(language="Swift", defined_in="Usabilla",generated_declaration))), apply_to=any(function,enum,objc_interface,objc_category,objc_protocol))
 # pragma pop_macro("any")
 #endif
+
+
 
 
 
