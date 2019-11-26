d84572774e65457415446fe23d08c97a9b5c83c4
Anders Liebl <anders.liebl@usabilla.com>
2019-11-25 20:16:53 -0500

    Add test for cancel of campaign

---
+++ automation/UsabillaSystemTest/UsabillaSystemTestUITests/03_TestForMultipleCampaignsCrash.swift
@@ -0,0 +1,50 @@
+//
+//  03_TestForMultipleCampaignsCrash.swift
+//  UsabillaSystemTestUITests
+//
+//  Created by Anders Liebl on 22/11/2019.
+//  Copyright © 2019 Usabilla. All rights reserved.
+//
+
+import XCTest
+
+class TestForMultipleCampaignsCrash: XCTestCase {
+    
+    let app = XCUIApplication()
+    
+    override func setUp() {
+        super.setUp()
+        app.launchArguments += ["-AppleLanguages", "(nl)"]
+        app.launchArguments += ["-AppleLocale", "nl_NL"]
+        continueAfterFailure = false
+        app.launch()
+    }
+    
+    func testCampaignAppearance() {
+        app.buttons["reset"].tap()
+        
+        app.textFields["eventInput"].tap()
+        app.textFields["eventInput"].typeText("triggerExtra")
+        app.windows.firstMatch.coordinate(withNormalizedOffset: CGVector(dx: 10, dy: 10)).tap()
+        var sendButton = app.buttons["send"]
+        sendButton.tap()
+        var ratingcontrolElement = app.otherElements["ratingControl"]
+        XCTAssert(!ratingcontrolElement.exists, "rating control banner does not exists")
+        
+        
+        let cancelButton = app.buttons["Cancel"]
+        cancelButton.tap()
+        
+        app.buttons["reset"].tap()
+        
+        app.textFields["eventInput"].tap()
+        app.textFields["eventInput"].typeText("triggerExtra")
+        app.windows.firstMatch.coordinate(withNormalizedOffset: CGVector(dx: 10, dy: 10)).tap()
+        sendButton = app.buttons["send"]
+        sendButton.tap()
+        ratingcontrolElement = app.otherElements["ratingControl"]
+        XCTAssert(!ratingcontrolElement.exists, "rating control banner does not exists")
+        
+    }
+    
+}
