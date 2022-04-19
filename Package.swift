af4cbfb861405f51e558669089cde77646f52033
Hitesh Jain <jainhitesh999@gmail.com>
2022-04-19 15:36:54 +0200

    Release 6.11.1

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@
 
 import PackageDescription
 
-let version = "6.11.0-Xcode-11.3.1"
-let checksum = "c10493923e4adcf05d6c1653d5faf0f89ddc27cfeda1aa41880b909d0f5ad60d"
+let version = "6.11.1-Xcode-11.3.1"
+let checksum = "06c10fb54d00be63f2a881bb555d052260d8396cf20b070d0c8a9abb8d44920a"
 let url = "https://github.com/usabilla/usabilla-u4a-ios-swift-sdk/releases/download/v\(version)/UsabillaXCFramework.zip"
 
 let package = Package(
