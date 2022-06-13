756d41a9cbe3a0a8143c3a94ede059776cabeeba
Hitesh Jain <jainhitesh999@gmail.com>
2022-06-13 15:46:33 +0200

    Release 6.12.0

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@
 
 import PackageDescription
 
-let version = "6.11.1-Xcode-11.3.1"
-let checksum = "06c10fb54d00be63f2a881bb555d052260d8396cf20b070d0c8a9abb8d44920a"
+let version = "6.12.0-Xcode-11.3.1"
+let checksum = "865fc6ce131f1078709f944892d352260bec80139cf16b1bbd9a86fe6ffe41ca"
 let url = "https://github.com/usabilla/usabilla-u4a-ios-swift-sdk/releases/download/v\(version)/UsabillaXCFramework.zip"
 
 let package = Package(
--- Usabilla.podspec
+++ Usabilla.podspec
@@ -1,4 +1,4 @@
-version = "6.11.1-Xcode-11.3.1"
+version = "6.12.0-Xcode-11.3.1"
 
 Pod::Spec.new do |s|
 
