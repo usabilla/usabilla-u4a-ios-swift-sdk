28bb7ce6c1a5e037d7b891b6e74f38430e4e425c
Anders Liebl <anders@liebl.dk>
2024-03-20 08:41:53 +0100

    Changed code to pure function for better testability

--- Usabilla/Stores/FormStore.swift
+++ Usabilla/Stores/FormStore.swift
@@ -16,6 +16,25 @@ class FormStore {
         self.formService = service
     }
 
+    private func addScreenShot(model: FormModel, image: UIImage? ) -> FormModel {
+        guard let screenShotImage = image else {
+            return model
+        }
+        var imageAdded = false
+        model.pages.forEach {
+            $0.fields.forEach {
+                if let model = $0 as? ScreenshotModel {
+                    model.image = screenShotImage
+                    imageAdded = true
+                }
+            }
+        }
+        if !imageAdded {
+            Swift.debugPrint("Usabilla: failed adding a provided screenshot image to the store form, as no screenshot module was found on the form")
+        }
+        return model
+
+    }
     // Entry to laod a form from Network or try getting it from Cache
     func loadForm(id: String, screenshot: UIImage?, theme: UsabillaTheme, maskModel: MaskModel?) -> Promise<FormModel> {
         return Promise { fulfill, reject in
@@ -27,10 +46,10 @@ class FormStore {
                 PLog("  FormModel is loaded successfully")
                 fulfill(form)
             }.catch { error in
-                if let cachedForm = UBFormDAO.shared.read(id: id) {
+                if var cachedForm = UBFormDAO.shared.read(id: id) {
                     cachedForm.theme = theme
                     cachedForm.updateTheme()
-                    cachedForm.addScrenshot(image: screenshot)
+                    cachedForm = self.addScreenShot(model: cachedForm, image: screenshot)
                     PLog("FormModel is loaded successfully")
                     fulfill(cachedForm)
                 } else {
